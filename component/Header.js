d943e7c003b331f8e61c8f85889b4928cb547719
Ravi kumar <rk5892962@gmail.com>
2023-12-19 23:46:24 +0530

    first commit

--- component/Header.js
+++ component/Header.js
@@ -1,47 +1,38 @@
-/* eslint-disable jsx-a11y/alt-text */
 "use client"
-import React from 'react';
-import {Row, Col, Container, Card, Nav} from 'react-bootstrap'
-import Image from 'next/image';
-import Link from 'next/link';
-import { Navbar, NavDropdown } from 'react-bootstrap';
-import { images } from '@/next.config';
-function Header() {
-  return (
-    <>
-  <Navbar  expand="lg" className=" shadow-sm px-5 bg-white  ">
-      <Navbar.Brand href="#home">
-        <Image src= "./images/cac_Logo.svg" width={190} height={80}></Image>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarNav" />
-      <Navbar.Collapse id="navbarNav">
-        <Nav className="ms-auto navbar-nav mx-4 ">
-          <Nav.Link className='px-2' href="/">Home</Nav.Link>
-          <Nav.Link className='px-2' href="/about">About us</Nav.Link>
-          <Nav.Link  className='px-2'href="/action">Our Action</Nav.Link>
-          <Nav.Link  className='px-2'href="/impact">Our Impact</Nav.Link>
-          <NavDropdown  className='px-2' title="Our Initiatives" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/shilp">SHILP</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/2">Economic resilience</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3">VaxNow</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/4">Precision Health</NavDropdown.Item>
-          </NavDropdown>
-          <NavDropdown className='px-2' title="Resource Hub" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/1">Knowledgebase</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item className='' href="#action/2">Media</NavDropdown.Item>
-            <NavDropdown.Divider />
-          </NavDropdown>
-          <Nav.Link href="/join-us">Join Us</Nav.Link>
-          <Nav.Link href="/contact-us">Contact Us </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-    </>
-  )
-}
+import { Navbar, Container, Nav, NavDropdown,Image } from 'react-bootstrap';
 
-export default Header
+const Header = () => {
+  return (
+    <Navbar bg="light" expand="lg" className='cac_main'>
+      <Container>
+        <Navbar.Brand href="#home">
+         <Image src='./images/cac_Logo.svg' width={214} height={50} alt='cac log'>
+
+         </Image>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto cac_nav_item">
+            <Nav.Link href="/" className='me-4'>Home</Nav.Link>
+            <Nav.Link href="/about" className='me-4'>About us</Nav.Link>
+            <Nav.Link href="action" className='me-4'>Our Action</Nav.Link>
+            <Nav.Link href="/impact" className='me-4'>Our Impact</Nav.Link>
+            <NavDropdown title="Our Initiatives" className='me-4' id="basic-nav-dropdown">
+              <NavDropdown.Item href="/shilp">SHILP</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/2">Economic resilience</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://covidactioncollab.org/vaxnow/" target='_blank'>VaxNow</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://www.precisionhealth.in/" target='_blank'>Precision Health</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/join-us/" className='me-4'>Join</Nav.Link>
+            <Nav.Link href="/contact-us/" className='me-4'>Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
