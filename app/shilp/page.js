152cfe6de92da8e97f26107bb80ddd73cab37394
Ravi kumar <rk5892962@gmail.com>
2024-01-05 11:48:13 +0530

    new page

--- app/shilp/page.js
+++ app/shilp/page.js
@@ -5,6 +5,11 @@ import Aboutshilp from './Shilpcac/Aboutshilp'
 import Shilpdetails from './Shilpcac/Shilpdetails'
 import Footer from '@/component/Footer'
 
+export const metadata = {
+  title: 'Empowering community organisations to progress in their pursuit for the last mile',
+  description: ' We help community organisations to network, learn and access opportunities through a framework of resources that could help them understand where they are at and advance their goals that could improve the quality of their work for the last mile.',
+}
+
 function page() {
   return (
     <>
